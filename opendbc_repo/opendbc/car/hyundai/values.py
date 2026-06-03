388beece7e0829857be2805627131f1e6112d492
bin9208 <bin920832@gmail.com>
2026-06-03 19:23:37 +0900

    Tighten K5 driver steering override limits

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -54,11 +54,11 @@ class CarControllerParams:
 
       if CP.carFingerprint in (CAR.KIA_K5_DL3_24, CAR.KIA_K5_DL3_24_HEV):
         self.STEER_MAX = 409
-        self.STEER_DRIVER_ALLOWANCE = 700
-        self.STEER_DRIVER_MULTIPLIER = 1
-        self.STEER_THRESHOLD = 650
-        self.STEER_DELTA_UP = 5
-        self.STEER_DELTA_DOWN = 12
+        self.STEER_DRIVER_ALLOWANCE = 250
+        self.STEER_DRIVER_MULTIPLIER = 2
+        self.STEER_THRESHOLD = 250
+        self.STEER_DELTA_UP = 3
+        self.STEER_DELTA_DOWN = 7
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
     # If the max stock LKAS request is <384, add your car to this list.
