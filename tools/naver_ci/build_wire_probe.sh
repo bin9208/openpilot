30340ced841fac09571185acf2aae917c65d3a57
bin9208 <bin920832@gmail.com>
2026-09-24 22:09:16 +0900

    test: include generated Capnp import directory in wire probe
    Refs #3. CI setup failure: generated log header imports car.capnp.h via angle brackets.

--- tools/naver_ci/build_wire_probe.sh
+++ tools/naver_ci/build_wire_probe.sh
@@ -12,7 +12,7 @@ mkdir -p "$build/cereal/gen/cpp"
   cd opendbc_repo/opendbc/car
   capnp compile -I . -I include -oc++:"$build/cereal/gen/cpp" car.capnp
 )
-g++ -std=c++17 -O1 -I "$repo/openpilot" -I "$build" \
+g++ -std=c++17 -O1 -I "$repo/openpilot" -I "$build" -I "$build/cereal/gen/cpp" \
   tools/naver_ci/wire_probe.cc \
   openpilot/selfdrive/carrot/realtime/compact_state_native.cc \
   "$build"/cereal/gen/cpp/*.capnp.c++ -lcapnp -lkj -o "$build/wire-probe"
